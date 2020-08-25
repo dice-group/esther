799dd76cb108580ee26418c853cf6195bb19afd8
aalexandrams@gmail.com <aalexandrams@gmail.com>
2020-08-25 12:03:34 +0200

    Exception handling removed - unnecessary

--- src/main/java/org/dice_group/graph_search/algorithms/AStarSearch.java
+++ src/main/java/org/dice_group/graph_search/algorithms/AStarSearch.java
@@ -5,29 +5,36 @@ import java.util.Queue;
 
 import org.dice_group.path.Graph;
 import org.dice_group.path.Node;
-import org.dice_group.path.exceptions.NoPathFoundException;
 
-public class AStarSearch implements SearchAlgorithm{
-	
+public class AStarSearch implements SearchAlgorithm {
+
 	@Override
-	public Node findOtherPaths(Graph graph, int sourceID, int edgeID, int destID, double[][] entitites, double[][] relations) throws NoPathFoundException{
+	public Node findOtherPaths(Graph graph, int sourceID, int edgeID, int destID, double[][] entitites,
+			double[][] relations)  {
 		Queue<Node> queue = new PriorityQueue<Node>();
 		queue.add(new Node(sourceID));
-		
-		while(!queue.isEmpty()) {
+
+		while (!queue.isEmpty()) {
 			// get first and remove it from queue
 			Node node = queue.poll();
-			
+
 			// goal node reached, return node
-			if(node.getNodeID() == destID) {
+			if (node.getNodeID() == destID) {
 				return node;
 			}
-			
+
 			// get in and outgoing edges (we want to look for paths in both directions)
 			queue.addAll(graph.getUndirectedSuccessors(node));
-		
+
+			// TODO check if infinite loop is created by selecting the same node from 2
+			// different directions, what is an acceptable stopping condition then?
+			
+			// TODO will the optimal path be among the selected? does this even matter?
+			
+			
+
 		}
-		throw new NoPathFoundException("Could not reach end node "+ destID);
+		return null;
 	}
 
 }
