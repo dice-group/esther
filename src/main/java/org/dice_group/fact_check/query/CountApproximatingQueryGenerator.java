83a34290f12cc76fbfeb404c814a410324fcc497
aalexandrams@gmail.com <aalexandrams@gmail.com>
2020-11-26 09:22:28 +0100

    Query adjustment.

--- src/main/java/org/dice_group/fact_check/query/CountApproximatingQueryGenerator.java
+++ src/main/java/org/dice_group/fact_check/query/CountApproximatingQueryGenerator.java
@@ -13,6 +13,7 @@ public class CountApproximatingQueryGenerator implements QueryGenerator {
 
     protected static final String COUNT_VARIABLE_NAME = "?sum";
     protected static final String INTERMEDIATE_COUNT_VARIABLE_NAME = "?c";
+    protected static final String INTERMEDIATE_COUNT2_VARIABLE_NAME = "?l";
     protected static final String INTERMEDIATE_NODE_VARIABLE_NAME = "?x";
     protected static final String SUBJECT_VARIABLE_NAME = INTERMEDIATE_NODE_VARIABLE_NAME + "0";
     protected static final String OBJECT_VARIABLE_NAME = "?o";
@@ -71,7 +72,12 @@ public class CountApproximatingQueryGenerator implements QueryGenerator {
         StringBuilder queryBuilder = new StringBuilder();
         // This is the first property in the list
         queryBuilder.append("Select (coalesce(sum(" + INTERMEDIATE_COUNT_VARIABLE_NAME);
-        queryBuilder.append("0*" + INTERMEDIATE_COUNT_VARIABLE_NAME);
+        queryBuilder.append("0*");
+        if(pathProperties.size() > 2) {
+        	queryBuilder.append(INTERMEDIATE_COUNT2_VARIABLE_NAME);
+        } else {
+        	queryBuilder.append(INTERMEDIATE_COUNT_VARIABLE_NAME);
+        }
         queryBuilder.append("1), 0) as " + COUNT_VARIABLE_NAME + ") where { \n");
         // Recursion
         createPropertyQuery_Recursion(0, pathProperties, propURIs, sTypeTriples, oTypeTriples, queryBuilder);
@@ -102,21 +108,27 @@ public class CountApproximatingQueryGenerator implements QueryGenerator {
             queryBuilder.append(propId);
             queryBuilder.append(") " + INTERMEDIATE_NODE_VARIABLE_NAME);
             queryBuilder.append(propId);
+            
             // If we are at the position before the last position
             if (propId == pathProperties.size() - 2) {
                 // We simply select the count from the last position
                 queryBuilder.append(" " + INTERMEDIATE_COUNT_VARIABLE_NAME);
                 queryBuilder.append(propId + 1);
             } else {
-                // Calculate the product of the previous counts
-                // with propId=0, this would look like (?c1*?c2 as ?c1)
+             // Calculate the product of the previous counts
                 queryBuilder.append(" (" + INTERMEDIATE_COUNT_VARIABLE_NAME);
                 queryBuilder.append(propId + 1);
-                queryBuilder.append("*sum(" + INTERMEDIATE_COUNT_VARIABLE_NAME);
+                if(propId == pathProperties.size() - 3) {
+                	queryBuilder.append("*sum(" + INTERMEDIATE_COUNT_VARIABLE_NAME);
+                } else {
+                	queryBuilder.append("*sum(" + INTERMEDIATE_COUNT2_VARIABLE_NAME);
+                }
                 queryBuilder.append(propId + 2);
-                queryBuilder.append(") as " + INTERMEDIATE_COUNT_VARIABLE_NAME);
+                queryBuilder.append(") as " + INTERMEDIATE_COUNT2_VARIABLE_NAME);
                 queryBuilder.append(propId + 1);
                 queryBuilder.append(")");
+            	
+                
             }
             queryBuilder.append(" where { \n");
             addTriplePattern(pathProperties.get(propId), propURIs[propId], INTERMEDIATE_NODE_VARIABLE_NAME + propId,
